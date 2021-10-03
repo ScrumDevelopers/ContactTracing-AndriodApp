0758aeda895f6fc02f12f65765295f615227cd31
Utkarsh Singh <utkarshsingh2015@gmail.com>
2021-10-04 00:25:57 +0530

    App colors added

---
+++ src/Assets/AppColor.js
@@ -0,0 +1,12 @@
+export default {
+logoViolet:"",
+logoPink:"",
+ButtonPink:"DD4FB1",
+backgroundBlue:"E2F7FF",
+carBlue:"CEF6FF",
+safeTextGreen:"00C02A",
+safeBackgroundGreen:"BAFFC1",
+safeRed:"FF0101",
+safeBackgroundRed:"FFCECE"
+
+} 
